8e25ebc0fbe03fe4f63142386dea5c50868c79a7
Joe Rivett <joe.rivett@songkick.com>
2018-01-18 22:39:02 +0000

    Memoising API results and added failure retry code to help with intermittent service failures

--- app/models/user_playlist.rb
+++ app/models/user_playlist.rb
@@ -21,51 +21,71 @@ class UserPlaylist
       return
     end
 
-    spotify = ::Services::SpotifyApi.new(@spotify_auth)
+    max_tries = 3
 
-    spotify_artists = get_spotify_artists(spotify)
-    playlist_tracks = get_top_tracks(spotify_artists, spotify)
+    begin
+      spotify = ::Services::SpotifyApi.new(@spotify_auth)
 
-    # Create user playlist
-    @generated_playlist = build_playlist(playlist_tracks, spotify)
-  rescue Services::SongkickApi::APIError => e
-    message = JSON.parse(e.message)['message']
-    case message
-    when 'Resource not found'
-      @errors << "There was an getting your Songkick concert history. Is your username correct?"
-    else
-      @errors << "There was an getting your Songkick concert history. Please try again later"
+      spotify_artists = get_spotify_artists(spotify)
+      playlist_tracks = get_top_tracks(spotify_artists, spotify)
+
+      # Create user playlist
+      @generated_playlist = build_playlist(playlist_tracks, spotify)
+    rescue Services::SongkickApi::APIError => e
+      message = JSON.parse(e.message)['message']
+      case message
+      when 'Resource not found'
+        @errors << "There was an getting your Songkick concert history. Is your username correct?"
+      else
+        @errors << "There was an getting your Songkick concert history. Please try again later"
+      end
+    rescue NoEventsError => e
+      @errors << "Looks like you haven't marked your attendance on any Songkick concerts"
+    rescue NoEventsInPastYearError => e
+      @errors << "Looks like you didn't mark your attendance on any Songkick concerts in 2017"
+    rescue RestClient::BadGateway, RestClient::ServerBrokeConnection => e
+      max_tries -= 1
+      if max_tries > 0
+        retry
+      else
+        puts "ERROR: #{e.inspect}"
+        @errors << "Something has gone terribly wrong but I don't know how to deal with it. Try one more time, maybe you'll get lucky."
+      end
+    rescue => e
+      puts "ERROR: #{e.inspect}"
+      @errors << "Something has gone terribly wrong but I don't know how to deal with it. Try one more time, maybe you'll get lucky."
     end
-  rescue NoEventsError => e
-    @errors << "Looks like you haven't marked your attendance on any Songkick concerts"
-  rescue NoEventsInPastYearError => e
-    @errors << "Looks like you didn't mark your attendance on any Songkick concerts in 2017"
-  rescue => e
-    @errors << "Something has gone terribly wrong but I don't know how to deal with it. Maybe try again?"
   end
 
   private
 
   # Get Spotify artist ids
   def get_spotify_artists(spotify)
-    spotify_artists = artist_names.reverse.map do |artist_name|
-      spotify.get_artist(artist_name)
-    end
+    @spotify_artists ||= begin
+      spotify_artists = artist_names.reverse.map do |artist_name|
+        spotify.get_artist(artist_name)
+      end
 
-    spotify_artists.reject(&:nil?)
+      spotify_artists.reject(&:nil?)
+    end
   end
 
   # Get top tracks for each artist
   def get_top_tracks(spotify_artists, spotify)
-    spotify_artists.map do |artist|
-      spotify.top_tracks(artist, :GB)
+    @top_tracks ||= begin
+      spotify_artists.map do |artist|
+        spotify.top_tracks(artist, :GB)
+      end
     end
   end
 
   def build_playlist(playlist_tracks, spotify)
     playlist = spotify.create_playlist(PLAYLIST_NAME)
-    # Add to user playlist
-    spotify.add_tracks_to_playlist(playlist, playlist_tracks.flatten.map(&:uri))
+    track_uris = playlist_tracks.flatten.map(&:uri)
+    # Add to user playlist in batches (minimise request URL length)
+    track_uris.each_slice(10) do |tracks|
+      spotify.add_tracks_to_playlist(playlist, tracks)
+    end
     playlist
   end
 
@@ -73,9 +93,10 @@ class UserPlaylist
   # TODO: strip country suffix?
   def artist_names
     @artist_names ||= begin
-      previous_year_concerts.map do |event|
+      names = previous_year_concerts.map do |event|
         event.headliners
-      end.flatten
+      end
+      names.flatten.uniq
     end
   end
 
@@ -94,9 +115,11 @@ class UserPlaylist
   end
 
   def user_concerts
-    events = ::Services::SongkickApi.gigography(@sk_username, Date.strptime('2017-01-01', '%Y-%m-%d'))
-    # Only concerts
-    events.reject { |event| event.festival? }
+    @user_concerts ||= begin
+      events = ::Services::SongkickApi.gigography(@sk_username, Date.strptime('2017-01-01', '%Y-%m-%d'))
+      # Only concerts
+      events.reject { |event| event.festival? }
+    end
   end
 
 end
