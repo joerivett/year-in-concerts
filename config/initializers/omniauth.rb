e7cc0d83c92f732baa84e9e7b545f32c925338ad
Joe Rivett <joe.rivett@songkick.com>
2019-12-07 13:30:59 +0000

    Fix issue with Oauth2 + RSpotify integration after Ruby upgrade
    The Omniauth2 gem used by RSpotify for Oauth authentication with
    the Spotify API was updated in line with the latest Ruby version
    but there were breaking changes. The auto-generation of the
    callback_url was removed which means clients must define this
    themselves now, see
    https://github.com/guilhermesad/rspotify/issues/189

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,10 @@ OmniAuth.config.on_failure = Proc.new do |env|
   PlaylistGeneratorController.action(:omniauth_failure).call(env)
 end
 
+Rails.application.config.to_prepare do
+  OmniAuth::Strategies::Spotify.include SpotifyOmniauthExtension
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, Services::SpotifyApi::CLIENT_ID, Services::SpotifyApi::CLIENT_SECRET, scope: 'playlist-modify-private'
 end
